e66dfeb20a4f2936d8a579b40f935500f826d242
John Li <liqin@hotmail.sg>
2026-08-24 17:24:12 +0800

    check_oanda_account

--- check_oanda_account.sh
+++ check_oanda_account.sh
@@ -1,3 +1,3 @@
 source .env
-curl -H "Authorization: Bearer $OANDA_API_TOKEN_LIVE" https://api-fxtrade.oanda.com/v3/accounts
+#curl -H "Authorization: Bearer $OANDA_API_TOKEN_LIVE" https://api-fxtrade.oanda.com/v3/accounts
 curl -H "Authorization: Bearer $OANDA_API_TOKEN" https://api-fxpractice.oanda.com/v3/accounts
