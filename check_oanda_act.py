cbd111a6a08a102d0e17b6074003425c93c847eb
John Li <liqin@hotmail.sg>
2026-08-27 10:02:17 +0800

    v20260827

--- check_oanda_act.py
+++
@@ -1,7 +0,0 @@
-# inside check_oanda_account.py
-
-from utils.oanda_execution import api
-
-print("API object:", api)
-print("Environment:", api.environment)
-print("Token prefix:", api.access_token[:10])
